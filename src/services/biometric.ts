83a42b6d1a832aac95d232d5a531517df0e68ffd
Pasindu Yeshan Abeysinghe <pasindua.18@cse.mrt.ac.lk>
2022-07-06 15:00:18 +0530

    React native biometric prompt enable

--- src/services/biometric.ts
+++ src/services/biometric.ts
@@ -1,18 +1,34 @@
-import ReactNativeBiometrics, { BiometryTypes } from 'react-native-biometrics'
+import ReactNativeBiometrics, {BiometryTypes} from 'react-native-biometrics';
+const rnBiometrics = new ReactNativeBiometrics();
 
-const rnBiometrics = new ReactNativeBiometrics()
-
-rnBiometrics.isSensorAvailable()
-  .then((resultObject) => {
-    const { available, biometryType } = resultObject
+export async function checkBiometricsAvailability() {
+  rnBiometrics.isSensorAvailable().then(resultObject => {
+    const {available, biometryType} = resultObject;
 
     if (available && biometryType === BiometryTypes.TouchID) {
-      console.log('TouchID is supported')
+      console.log('TouchID is supported');
     } else if (available && biometryType === BiometryTypes.FaceID) {
-      console.log('FaceID is supported')
+      console.log('FaceID is supported');
     } else if (available && biometryType === BiometryTypes.Biometrics) {
-      console.log('Biometrics is supported')
+      console.log('Biometrics is supported');
     } else {
-      console.log('Biometrics not supported')
+      console.log('Biometrics not supported');
     }
-  })
+  });
+}
+
+export async function promptForBiometric() {
+  try {
+    const resultObject = await rnBiometrics.simplePrompt({
+      promptMessage: 'Confirm fingerprint',
+    });
+    if (resultObject.success) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
